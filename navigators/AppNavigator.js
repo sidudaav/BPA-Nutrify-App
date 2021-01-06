1e737365808974f549f7b9d6ea693688a3d751ee
Sid Jha <sidjha03@gmail.com>
2021-01-05 21:29:09 -0500

    styled tab bar and added header icon

--- navigators/AppNavigator.js
+++ navigators/AppNavigator.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { Ionicons } from '@expo/vector-icons';
+
+import { Colors } from '../constants/Theme';
+
 import HomeNavigator from './HomeNavigator';
 import ProfileNavigator from './ProfileNavigator';
 
@@ -8,11 +12,45 @@ const AppTabs = createBottomTabNavigator();
 
 const AppNavigator = () => {
     return (
-        <AppTabs.Navigator>
-            <AppTabs.Screen name="Home" component={HomeNavigator} />
+        <AppTabs.Navigator
+            tabBarOptions={{
+                activeTintColor: Colors.tertiaryGreen,
+                showLabel: false,
+            }}
+        >
+            <AppTabs.Screen
+                name="Home"
+                component={HomeNavigator}
+                options={{
+                    tabBarIcon: (props) => {
+                        return (
+                            <Ionicons
+                                name="md-home"
+                                size={30}
+                                color={
+                                    props.focused ? 'green' : 'black'
+                                }
+                            />
+                        );
+                    },
+                }}
+            />
             <AppTabs.Screen
                 name="Profile"
                 component={ProfileNavigator}
+                options={{
+                    tabBarIcon: (props) => {
+                        return (
+                            <Ionicons
+                                name="md-person"
+                                size={30}
+                                color={
+                                    props.focused ? 'green' : 'black'
+                                }
+                            />
+                        );
+                    },
+                }}
             />
         </AppTabs.Navigator>
     );
--- navigators/HomeNavigator.js
+++ navigators/HomeNavigator.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from '../screens/HomeScreen';
 
@@ -8,7 +11,26 @@ const HomeStack = createStackNavigator();
 const HomeNavigator = () => {
     return (
         <HomeStack.Navigator>
-            <HomeStack.Screen name="Home" component={HomeScreen} />
+            <HomeStack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    headerRight: () => (
+                        <TouchableOpacity
+                            style={{
+                                marginRight: 10,
+                            }}
+                            activeOpacity={0.6}
+                        >
+                            <Ionicons
+                                name="ios-add-circle-outline"
+                                size={30}
+                                color="black"
+                            />
+                        </TouchableOpacity>
+                    ),
+                }}
+            />
         </HomeStack.Navigator>
     );
 };
